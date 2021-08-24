b0aba23e428748f1ab3ae8467855f694ca0112d5
Yurij M <Yurij2015@users.noreply.github.com>
2021-08-24 18:04:14 +0300

    get project resources

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return new ProjectCollection(Project::all());
+        return ProjectResource::collection(Project::all());
     }
 
     /**
