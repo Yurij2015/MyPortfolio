4949c2ab0a3c45ffbdf12c0433a789d04a424780
Yurij <nabor_2008@i.ua>
2021-08-25 12:36:35 +0300

    set routes

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,12 +1,16 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import Dashboard from '@/pages/Dashboard.vue'
+import Projects from '@/pages/Projects.vue'
 
 const routes: Array<RouteRecordRaw> = [
+    {path: '', component: Dashboard},
+    {path: '/projects', component: Projects},
 
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
