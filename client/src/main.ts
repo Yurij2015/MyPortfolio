5dfbeb160fde2de52bfb454d268dda56fad06da1
Yurij M <Yurij2015@users.noreply.github.com>
2021-08-30 07:06:09 +0300

    set acces to remote api server

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import axios from "axios"
 
-axios.defaults.baseURL = 'http://localhost:8000/api/'
+// axios.defaults.baseURL = 'http://localhost:8000/api/'
+axios.defaults.baseURL = 'http://portfolioapi.webstudio.890m.com/api/'
 createApp(App).use(router).mount('#app')
